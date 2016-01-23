2bfb5987e24dbb0ac0fa156d140ead70673e54f3
ysw <yswai1986@gmail.com>
2016-01-23 23:18:40 +0800

    init

---
+++ ml-stars/src/main/java/com/swyep/StarService.java
@@ -0,0 +1,30 @@
+package com.swyep;
+
+public class StarService {
+
+    public static String SYMBOL = "*";
+
+    public String getStars(int totalRows) {
+        StringBuilder str = new StringBuilder();
+        if (totalRows == 0) return "";
+        int columnLength = ((totalRows - 1) * 2) + 1;
+        for (int n = 0; n < totalRows; n++) {
+            Double sideLength = Math.floor(columnLength / 2.0d);
+            int contentLength = (n * 2) + 1;
+            str.append(replicateStringByTimes(" ", sideLength.intValue()))
+                    .append(replicateStringByTimes(SYMBOL, contentLength))
+                    .append(replicateStringByTimes(" ", sideLength.intValue()))
+                    .append("\n");
+        }
+        return str.toString();
+    }
+
+    private String replicateStringByTimes(String s, int times) {
+        StringBuilder str = new StringBuilder();
+        for (int i = 0 ; i < times ; i++) {
+            str.append(s);
+        }
+        return str.toString();
+    }
+
+}
---
+++ ml-stars/src/test/java/com/swyep/StarServiceTest.java
@@ -0,0 +1,29 @@
+package com.swyep;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StarServiceTest {
+
+    private StarService starService;
+
+    @Before
+    public void setup() {
+        starService = new StarService();
+    }
+
+    @Test
+    public void getCorrectStars() {
+        System.out.println("--------------------");
+        System.out.println(starService.getStars(1));
+        System.out.println("--------------------");
+        System.out.println(starService.getStars(2));
+        System.out.println("--------------------");
+        System.out.println(starService.getStars(3));
+        System.out.println("--------------------");
+        System.out.println(starService.getStars(4));
+        System.out.println("--------------------");
+        System.out.println(starService.getStars(5));
+    }
+
+}
